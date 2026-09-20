9d5a77afaf4d555f890a68fe5f008628872014f9
adzra <2007adzranailalhusna@gmail.com>
2026-09-20 20:52:01 +0800

    Menambahkan route halo

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/mahasiswa', function () {
     return "halaman mahasiswa";
 });
+
+Route::get('/halo', function () {
+    return "Halo dari Laravel!";
+});
